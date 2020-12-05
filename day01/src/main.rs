824ea377b4182329b9de3e3d727507b08b190acd
Michael Aquilina <michaelaquilina@gmail.com>
2020-12-05 12:31:21 +0000

    Add solution for day 01 part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -13,20 +13,39 @@ fn main() -> Result<(), Box<dyn Error>> {
         values.push(value);
     }
 
-    if let Some((v1, v2)) = find_result(&values) {
-        println!("{} * {} = {}", v1, v2, v1 * v2);
+    if let Some((v1, v2)) = find_result_pairs(&values) {
+        println!("Part 1: {} * {} = {}", v1, v2, v1 * v2);
     } else {
-        println!("Unable to find result!");
+        println!("Unable to find result for part 1!");
+    }
+
+    if let Some((v1, v2, v3)) = find_result_triples(&values) {
+        println!("Part 2: {} * {} * {} = {}", v1, v2, v3, v1 * v2 * v3);
+    } else {
+        println!("Unable to find result for part 2!");
     }
 
     Ok(())
 }
 
-fn find_result(values: &[i32]) -> Option<(i32, i32)> {
+fn find_result_pairs(values: &[i32]) -> Option<(i32, i32)> {
     for v1 in values {
         for v2 in values {
             if v1 + v2 == 2020 {
                 return Some((*v1, *v2));
+            }
+        }
+    }
+    None
+}
+
+fn find_result_triples(values: &[i32]) -> Option<(i32, i32, i32)> {
+    for v1 in values {
+        for v2 in values {
+            for v3 in values {
+                if v1 + v2 + v3 == 2020 {
+                    return Some((*v1, *v2, *v3));
+                }
             }
         }
     }
