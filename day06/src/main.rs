fc12c9d39b54f02a28970963afc1fde1ebf531b8
Michael Aquilina <michaelaquilina@gmail.com>
2020-12-06 20:38:20 +0000

    Add solution for day 06 part 2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::error::Error;
 use std::io::Read;
 
@@ -7,8 +7,17 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     std::io::stdin().read_to_string(&mut contents)?;
 
-    let mut count = 0;
+    let count = part1(&contents);
+    println!("Part 1: {}", count);
 
+    let count = part2(&contents);
+    println!("Part 2: {}", count);
+
+    Ok(())
+}
+
+fn part1(contents: &str) -> usize {
+    let mut count = 0;
     for group in contents.split("\n\n") {
         let mut answers = HashSet::new();
         for line in group.lines() {
@@ -19,7 +28,25 @@ fn main() -> Result<(), Box<dyn Error>> {
         count += answers.len();
     }
 
-    println!("Part 1: {}", count);
+    count
+}
 
-    Ok(())
+fn part2(contents: &str) -> usize {
+    let mut count = 0;
+    for group in contents.split("\n\n") {
+        let mut answers: HashMap<char, u32> = HashMap::new();
+        let mut group_size = 0;
+        for line in group.lines() {
+            group_size += 1;
+
+            for answer in line.chars() {
+                let value = answers.entry(answer).or_insert(0);
+                *value += 1;
+            }
+        }
+
+        count += answers.values().filter(|v| *v == &group_size).count();
+    }
+
+    count
 }
