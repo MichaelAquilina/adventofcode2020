117cef15b6d076536ad0b2817e5c1d08bda3c7ab
Michael Aquilina <michaelaquilina@gmail.com>
2020-12-06 18:49:21 +0000

    add solution for day 05 part 2

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -8,16 +8,21 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     std::io::stdin().read_to_string(&mut contents)?;
 
-    let mut seat_ids = vec![];
+    let mut seats = vec![];
 
     for entry in contents.lines() {
         let (row, column) = parse_boarding_pass(entry).context(String::from(entry))?;
 
         let seat_id = get_seat_id(row, column);
-        seat_ids.push(seat_id);
+        seats.push(seat_id);
     }
+    seats.sort_unstable();
 
-    println!("Part 1: {:?}", seat_ids.iter().max());
+    println!("Part 1: {}", seats.last().ok_or("No data")?);
+
+    let missing = find_missing(&seats).ok_or("No data")?;
+
+    println!("Part 2: {}", missing);
 
     Ok(())
 }
@@ -37,4 +42,19 @@ fn parse_boarding_pass(entry: &str) -> Result<(u32, u32), ParseIntError> {
 
 fn get_seat_id(row: u32, column: u32) -> u32 {
     (row * 8) + column
+}
+
+// TODO: There must be a more elegant way to do this
+fn find_missing(seats: &[u32]) -> Option<u32> {
+    if let Some(first) = seats.first() {
+        let mut current = *first;
+        for seat in seats {
+            if seat != &current {
+                return Some(current);
+            }
+
+            current += 1;
+        }
+    }
+    None
 }
