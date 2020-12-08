b8e71defe654b7f14465c384bfb6ab52bde6c1af
Michael Aquilina <michaelaquilina@gmail.com>
2020-12-08 22:46:47 +0000

    Add solution for day07 part 2

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -2,7 +2,21 @@ use std::collections::{HashMap, HashSet};
 use std::error::Error;
 use std::io::Read;
 
-type BagMap = HashMap<String, Vec<(String, usize)>>;
+type BagMap = HashMap<String, Bag>;
+
+struct Bag {
+    parents: Vec<String>,
+    children: Vec<(String, usize)>,
+}
+
+impl Bag {
+    fn new() -> Self {
+        Bag {
+            parents: vec![],
+            children: vec![],
+        }
+    }
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut contents = String::new();
@@ -13,18 +27,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     let count = traverse(&bags, "shiny gold");
 
     println!("Part 1: {}", count.len());
+    println!("Part 2: {}", traverse_2(&bags, "shiny gold"));
 
     Ok(())
 }
 
 fn traverse(bags: &BagMap, value: &str) -> HashSet<String> {
     let mut result = HashSet::new();
-    if let Some(parents) = bags.get(value) {
-        for (parent, _) in parents {
+    if let Some(bag) = bags.get(value) {
+        for parent in &bag.parents {
             result.insert(String::from(parent));
 
             // merge output from traverse into this result
-            for parent in traverse(bags, &parent) {
+            for parent in traverse(bags, parent) {
                 result.insert(parent);
             }
         }
@@ -33,12 +48,23 @@ fn traverse(bags: &BagMap, value: &str) -> HashSet<String> {
     result
 }
 
+fn traverse_2(bags: &BagMap, value: &str) -> usize {
+    let mut result = 0;
+    if let Some(bag) = bags.get(value) {
+        for (child, count) in &bag.children {
+            result += count + (count * traverse_2(bags, child));
+        }
+    }
+
+    result
+}
+
 fn parse_bags(content: &str) -> Result<BagMap, Box<dyn Error>> {
-    let mut bags = HashMap::new();
+    let mut bags: HashMap<String, Bag> = HashMap::new();
 
     for line in content.lines() {
         let mut tokens = line.split(" bags contain ");
-        let parent = tokens.next().ok_or("Missing bag color")?;
+        let parent_color = tokens.next().ok_or("Missing bag color")?;
         let children = tokens
             .next()
             .ok_or("Missing bag children")?
@@ -46,6 +72,7 @@ fn parse_bags(content: &str) -> Result<BagMap, Box<dyn Error>> {
             .unwrap();
 
         if children != "no other bags" {
+            let mut child_colors = vec![];
             for child in children.split(", ") {
                 // pretty hacky, could probably improve this string replace!
                 let child = child.replace(" bags", "").replace(" bag", "");
@@ -55,10 +82,16 @@ fn parse_bags(content: &str) -> Result<BagMap, Box<dyn Error>> {
                 let remaining_tokens = tokens.collect::<Vec<&str>>();
 
                 let child_color = remaining_tokens.join(" ");
-
-                let parents = bags.entry(child_color).or_insert(Vec::new());
-                parents.push((String::from(parent), number));
+                child_colors.push((child_color, number));
             }
+
+            for (child_color, _) in &child_colors {
+                let child_bag = bags.entry(String::from(child_color)).or_insert(Bag::new());
+                child_bag.parents.push(String::from(parent_color));
+            }
+
+            let parent_bag = bags.entry(String::from(parent_color)).or_insert(Bag::new());
+            parent_bag.children = child_colors;
         }
     }
 
@@ -88,6 +121,27 @@ mod test_traverse {
         let result = traverse(&bags, "shiny gold");
 
         assert_eq!(result.len(), 4);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_provided_example_part_2() -> Result<(), Box<dyn Error>> {
+        let content = vec![
+            "shiny gold bags contain 2 dark red bags.",
+            "dark red bags contain 2 dark orange bags.",
+            "dark orange bags contain 2 dark yellow bags.",
+            "dark yellow bags contain 2 dark green bags.",
+            "dark green bags contain 2 dark blue bags.",
+            "dark blue bags contain 2 dark violet bags.",
+            "dark violet bags contain no other bags.",
+        ]
+        .join("\n");
+
+        let bags = parse_bags(&content)?;
+        let result = traverse_2(&bags, "shiny gold");
+
+        assert_eq!(result, 126);
 
         Ok(())
     }
